bcde606fd2766613cd2b9dfd4de7bc2f1fe8cd22
Mikolaj Babianski <mikolaj395@gmail.com>
2020-02-02 11:39:54 +0100

    datasets preparation moved to utils

--- TensorFlowV2_CNN_MNIST/train.py
+++ TensorFlowV2_CNN_MNIST/train.py
@@ -1,16 +1,10 @@
 import tensorflow as tf
-import tensorflow_datasets as tfds
-from utils import scale, build_and_compile_cnn_model
+from utils import build_and_compile_cnn_model, prepare_train_datasets
 
-BUFFER_SIZE = 10000
 BATCH_SIZE = 64
 EPOCHS_NUM = 3
 
-datasets, info = tfds.load(name='mnist',
-                           with_info=True,
-                           as_supervised=True)
-train_datasets_unbatched = datasets['train'].map(scale).shuffle(BUFFER_SIZE)
-train_datasets = train_datasets_unbatched.batch(BATCH_SIZE)
+train_datasets = prepare_train_datasets(BATCH_SIZE)
 
 model = build_and_compile_cnn_model()
 model.fit(x=train_datasets, epochs=EPOCHS_NUM)
--- TensorFlowV2_CNN_MNIST/utils.py
+++ TensorFlowV2_CNN_MNIST/utils.py
@@ -1,4 +1,6 @@
 import tensorflow as tf
+import tensorflow_datasets as tfds
+from absl import app, flags
 
 
 # Scaling MNIST data from (0, 255] to (0., 1.]
@@ -6,6 +8,16 @@ def scale(image, label):
     image = tf.cast(image, tf.float32)
     image /= 255
     return image, label
+
+
+def prepare_train_datasets(batch_size):
+    BUFFER_SIZE = 10000
+    datasets, info = tfds.load(name='mnist',
+                               with_info=True,
+                               as_supervised=True)
+    train_datasets_unbatched = datasets['train'].map(scale).shuffle(BUFFER_SIZE)
+    train_datasets = train_datasets_unbatched.batch(batch_size)
+    return train_datasets
 
 
 def build_and_compile_cnn_model():
