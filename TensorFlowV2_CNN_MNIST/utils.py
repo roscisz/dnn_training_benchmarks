5e71aaadb9b8f81d3a7451dea4e397545127a344
Mikolaj Babianski <mikolaj395@gmail.com>
2020-02-02 11:22:15 +0100

    utils

---
+++ TensorFlowV2_CNN_MNIST/utils.py
@@ -0,0 +1,23 @@
+import tensorflow as tf
+
+
+# Scaling MNIST data from (0, 255] to (0., 1.]
+def scale(image, label):
+    image = tf.cast(image, tf.float32)
+    image /= 255
+    return image, label
+
+
+def build_and_compile_cnn_model():
+    model = tf.keras.Sequential([
+        tf.keras.layers.Conv2D(32, 3, activation='relu', input_shape=(28, 28, 1)),
+        tf.keras.layers.MaxPooling2D(),
+        tf.keras.layers.Flatten(),
+        tf.keras.layers.Dense(64, activation='relu'),
+        tf.keras.layers.Dense(10, activation='softmax')
+    ])
+    model.compile(
+        loss=tf.keras.losses.sparse_categorical_crossentropy,
+        optimizer=tf.keras.optimizers.SGD(learning_rate=0.001),
+        metrics=['accuracy'])
+    return model
