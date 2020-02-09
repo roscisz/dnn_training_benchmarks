5d8d68353752f3b12bd00a339a4842c078cd62fd
Mikolaj Babianski <mikolaj395@gmail.com>
2020-02-09 11:40:16 +0100

    Fix: Collective ops must be configured at program startup

--- TensorFlowV2_CNN_MNIST/train_multi_worker_mirrored_strategy.py
+++ TensorFlowV2_CNN_MNIST/train_multi_worker_mirrored_strategy.py
@@ -7,11 +7,12 @@ FLAGS = flags.FLAGS
 flags.DEFINE_integer('batch_size', 64, 'Batch size', lower_bound=0)
 flags.DEFINE_integer('epochs', 3, 'Number of epochs', lower_bound=0)
 
+strategy = tf.distribute.experimental.MultiWorkerMirroredStrategy()
+
 
 def main(argv):
     train_datasets = prepare_train_datasets(FLAGS.batch_size)
 
-    strategy = tf.distribute.experimental.MultiWorkerMirroredStrategy()
     with strategy.scope():
         model = build_and_compile_cnn_model()
     model.fit(x=train_datasets, epochs=FLAGS.epochs)
